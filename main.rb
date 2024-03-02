2153f5e8cb75b1609eed2370c95f9d5927830f49
KurniawanZATY <126676817+KurniawanZATY@users.noreply.github.com>
2024-03-02 13:17:04 +0700

    Update main.rb

--- main.rb
+++ main.rb
@@ -6,3 +6,7 @@ api_key = ENV['API_KEY']
 
 puts "API Key: #{api_key}"
 puts "Hello, world!".colorize(:green)
+
+# Add GitHub repository URL
+github_url = "https://github.com/#{ENV['GITHUB_REPOSITORY']}"
+puts "GitHub Repository URL: #{github_url}"
