5c31295bdef1fb70207728a3022e8b985e4ae999
KurniawanZATY <126676817+KurniawanZATY@users.noreply.github.com>
2024-03-02 12:26:41 +0700

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,2 +1,7 @@
 # main.rb
 puts "Hello, world!"
+
+# Accessing the RubyGems API key from the environment variable
+rubygems_api_key = ENV['RUBYGEMS_API_KEY']
+
+puts "RubyGems API Key: #{rubygems_api_key}"
